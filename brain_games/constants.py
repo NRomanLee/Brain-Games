35ac4429a8da04bd6e68b9e379004477efa7dd85
NRomanLee <leeroman680@gmail.com>
2024-04-18 01:21:36 +0300

    fix

--- brain_games/constants.py
+++ brain_games/constants.py
@@ -1,5 +1,5 @@
 GAME_INSTRUCTIONS = {
-    "even": 'Answer "yes" if the number is even, otherwise answer "no".',
+    "even": '''Answer "yes" if the number is even, otherwise answer "no".''',
     "prime": 'Answer "yes" if given number is prime. Otherwise answer "no".',
     "calc": '''What is the result of the expression?''',
     "gcd": '''Find the greatest common divisor of given numbers.''',

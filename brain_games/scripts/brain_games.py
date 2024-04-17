af057782342d7d10e923f48cd00ffd1e81f1679d
NRomanLee <164695760+NRomanLee@users.noreply.github.com>
2024-04-17 17:17:06 +0300

    Delete brain_games directory

--- brain_games/scripts/brain_games.py
+++
@@ -1,9 +0,0 @@
-def greet(greet_speech):
-    print(greet_speech)
-def main():
-    greet("Welcome to the Brain Games!")
-
-if __name__ == "__main__":
-    main()
-    import welcome_user
-    
--- brain_games/scripts/welcome_user.py
+++
@@ -1,3 +0,0 @@
-import prompt
-name = prompt.string("May I have your name? ")
-print("Hello, " + name + "!")

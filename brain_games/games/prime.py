b215301a2087ca9c18bab6d132deb02d7d4566a3
NRomanLee <leeroman680@gmail.com>
2024-04-21 05:10:37 +0300

    fix

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,4 +1,5 @@
 import random
+from brain_games.the_engine import run_game
 
 
 def is_prime(num):
@@ -11,8 +12,12 @@ def is_prime(num):
 
 
 def get_number_and_prime_answer():
-
-    number = random.randint(1, 20)
+    number = random.randint(1, 100)
     prime_check = is_prime(number)
     answer = 'yes' if prime_check else 'no'
-    return number, answer
+    return str(number), answer
+
+
+def play_prime():
+    run_game(get_number_and_prime_answer,
+             'Answer "yes" if given number is prime. Otherwise answer "no".')
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
-from brain_games.games.prime import get_number_and_prime_answer
-from brain_games.the_engine import run_game
 
-
-def play_prime():
-    run_game(get_number_and_prime_answer,
-             'Answer "yes" if given number is prime. Otherwise answer "no".')
+from brain_games.games.prime import play_prime
 
 
 def main():

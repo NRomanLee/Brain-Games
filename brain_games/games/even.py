53db270db8b4bdbbdc257b15f86cde89583e46ed
NRomanLee <leeroman680@gmail.com>
2024-04-21 05:16:23 +0300

    fix

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,4 +1,5 @@
 import random
+from brain_games.the_engine import run_game
 
 
 def is_even(num):
@@ -9,3 +10,8 @@ def get_num_and_even_answer():
     num = random.randint(1, 20)
     correct_answer = 'yes' if is_even(num) else 'no'
     return num, correct_answer
+
+
+def play_game():
+    run_game(get_num_and_even_answer,
+             '''Answer "yes" if the number is even, otherwise answer "no".''')
